799bb8af2b946b5262a6a7ed3c6d1744f3a0769a
ryan-tzw <102912851+ryan-tzw@users.noreply.github.com>
2026-09-17 07:26:27 +0800

    lock rotation when attacking

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -13,15 +13,17 @@ public class Player : MonoBehaviour
     [SerializeField] private float attackCooldown = 0.4f;
     private float _nextAttackTime;
 
+    private float _rotationLockedTime;
+
     private Rigidbody2D _rb;
+    
     private PlayerInput _playerInput;
     private InputActionMap _combatActions;
-
     private InputAction _moveAction;
     private InputAction _attackAction;
     private InputAction _aimAction;
+    
     private Vector2 facingDir = Vector2.right;
-
     private Vector2 _moveDir;
 
     private void Awake()
@@ -40,6 +42,7 @@ public class Player : MonoBehaviour
         if (Time.time < _nextAttackTime) return;
 
         _nextAttackTime = Time.time + attackCooldown;
+        _rotationLockedTime = Time.time + swingLifetime;
         
         Quaternion rotation = spriteTransform.rotation * Quaternion.Euler(0f, 0f, swingAngleOffset);
         GameObject swing =  Instantiate(swingPrefab, transform.position, rotation, transform);
@@ -53,38 +56,40 @@ public class Player : MonoBehaviour
             Attack();
         }
         
-        /* Aiming */
-        
-        Vector2 aim = _aimAction.ReadValue<Vector2>();
+        // only turn the player when not locked in an animation
+        if (Time.time >= _rotationLockedTime)
+        {
+            Vector2 aim = _aimAction.ReadValue<Vector2>();
 
-        // note: for now im just putting everything in a single file since it's still quite small but refactor later if it gets too large
-        // keyboard and mouse aiming
-        if (_playerInput.currentControlScheme == "KBM")
-        {
-            Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(aim);
-            facingDir = ((Vector2)mouseWorld - (Vector2)transform.position).normalized;
-        }
-        // controller aiming
-        else
-        {
-            // right stick aiming
-            if (aim.sqrMagnitude > 0.1f)
+            // note: for now im just putting everything in a single file since it's still quite small but refactor later if it gets too large
+            // keyboard and mouse aiming
+            if (_playerInput.currentControlScheme == "KBM")
             {
-                facingDir = aim.normalized;
+                Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(aim);
+                facingDir = ((Vector2)mouseWorld - (Vector2)transform.position).normalized;
             }
-            // use move direction if right stick not in use
+            // controller aiming
             else
             {
-                Vector2 move = _moveAction.ReadValue<Vector2>();
-                if (move.sqrMagnitude > 0.1f)
+                // right stick aiming
+                if (aim.sqrMagnitude > 0.1f)
                 {
-                    facingDir = move.normalized;
+                    facingDir = aim.normalized;
+                }
+                // use move direction if right stick not in use
+                else
+                {
+                    Vector2 move = _moveAction.ReadValue<Vector2>();
+                    if (move.sqrMagnitude > 0.1f)
+                    {
+                        facingDir = move.normalized;
+                    }
                 }
             }
-        }
 
-        float angle = Mathf.Atan2(facingDir.y, facingDir.x) * Mathf.Rad2Deg;
-        spriteTransform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
+            float angle = Mathf.Atan2(facingDir.y, facingDir.x) * Mathf.Rad2Deg;
+            spriteTransform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
+        }
     }
 
     private void FixedUpdate()
