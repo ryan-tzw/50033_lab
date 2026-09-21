0bf7507a3480aefc87a9da111094c089f2e4ab20
ryan-tzw <102912851+ryan-tzw@users.noreply.github.com>
2026-09-21 20:49:40 +0800

    death animation

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
     // health
     [SerializeField] private int maxHealth = 3;
     private int _currentHealth;
+    
+    // death/dissolve shader
+    [SerializeField] private float dissolveDuration = 1f;
+    private float _dissolveStartTime;
+    private  float _dissolveEndTime;
+    
+    // note:
+    // all instances use the same material so we can't modify the material itself or it will affect other instances
+    // MaterialPropertyBlock lets us override properties for individual instances
+    // (in this case i want to randomise the noise offset so the animation doesn't look the same every time)
+    private MaterialPropertyBlock _materialProperties;
+    private static readonly int DissolveAmountId = Shader.PropertyToID("_DissolveAmount");
+    private static readonly int NoiseOffsetId = Shader.PropertyToID("_NoiseOffset");
+    private static readonly int FlashAmountId = Shader.PropertyToID("_FlashAmount");
 
     // hitstun/knockback
-    [SerializeField] private float hitstunDuration = 0.5f;
-    [SerializeField] private float recoilSpeed = 15f;
+    [SerializeField] private float hitstunDuration = 0.25f;
+    [SerializeField] private float recoilSpeed = 25f;
     [SerializeField] private float recoilFalloffPower = 3f;
     private float _hitstunStartTime;
     private float _hitstunEndTime;
@@ -17,34 +32,47 @@ public class Enemy : MonoBehaviour
     // hitstop
     // todo: unify this between the player and the enemy.
     //  hitstop should probably be part of the attack's data since different attacks want different hitstop (light/heavy)
-    [SerializeField] private float hitstopDuration = 0.05f;
+    [SerializeField] private float hitstopDuration = 0.1f;
     private float _hitstopEndTime;
 
     private EnemyState _state = EnemyState.Alive;
     private Rigidbody2D _rb;
     private SpriteRenderer _spriteRenderer;
-    private Color _normalColor; // save the initial color to reset back to
+    private Collider2D _collider;
 
     private enum EnemyState
     {
         Alive,
         Hitstopped,
         Hitstunned,
+        Dying,
+        Dead
     }
 
     private void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        _normalColor =  _spriteRenderer.color;
         _rb = GetComponent<Rigidbody2D>();
         _currentHealth = maxHealth;
+        _collider = GetComponent<Collider2D>();
+        
+        // copy the renderer's existing properties first 
+        _materialProperties = new MaterialPropertyBlock();
+        _spriteRenderer.GetPropertyBlock(_materialProperties);
+        
+        // modify the properties with our random values, then update it 
+        _materialProperties.SetFloat(DissolveAmountId, 0f);
+        _materialProperties.SetVector(NoiseOffsetId, new Vector4(Random.Range(0f, 100f), Random.Range(0f, 100f), 0f, 0f));
+        _spriteRenderer.SetPropertyBlock(_materialProperties);
     }
-    
+
     public void TakeDamage(int damage, Vector2 attackerPosition)
     {
-        // todo: die when hp <= 0
+        if (_state is EnemyState.Dying or EnemyState.Dead) return;
+        
         _currentHealth -= damage;
-        _spriteRenderer.color = Color.white;
+        _materialProperties.SetFloat(FlashAmountId, 1f);
+        _spriteRenderer.SetPropertyBlock(_materialProperties);
         
         // save the recoil direction for later during hitstun phase
         _recoilDirection = (_rb.position - attackerPosition).normalized;
@@ -54,6 +82,30 @@ public class Enemy : MonoBehaviour
         _hitstopEndTime =  Time.time + hitstopDuration;
     }
 
+    private void Update()
+    {
+        switch (_state)
+        {
+            case EnemyState.Dying:
+            {
+                float progress = Mathf.InverseLerp(_dissolveStartTime, _dissolveEndTime, Time.time);
+                
+                _spriteRenderer.GetPropertyBlock(_materialProperties);
+                _materialProperties.SetFloat(DissolveAmountId, progress);
+                _spriteRenderer.SetPropertyBlock(_materialProperties);
+
+                if (progress >= 1f)
+                {
+                    _state = EnemyState.Dead;
+                    gameObject.SetActive(false);
+                }
+                
+                break;
+            }
+            
+        }
+    }
+
     private void FixedUpdate()
     {
         switch (_state)
@@ -61,28 +113,51 @@ public class Enemy : MonoBehaviour
             case EnemyState.Hitstopped:
             {
                 _rb.linearVelocity = Vector2.zero;
+                
+                // when hitstop ends
                 if (Time.time > _hitstopEndTime)
                 {
-                    // when hitstop ends, begin hitstun duration
-                    _state = EnemyState.Hitstunned;
-                    _hitstunStartTime =  Time.time;
+                    _materialProperties.SetFloat(FlashAmountId, 0f);
+                    _spriteRenderer.SetPropertyBlock(_materialProperties);
+                    
+                    _hitstunStartTime = Time.time;
                     _hitstunEndTime = Time.time + hitstunDuration;
                     _rb.linearVelocity = _recoilDirection * recoilSpeed;
+                    
+                    if (_currentHealth <= 0)
+                    {
+                        _state = EnemyState.Dying;
+                        _dissolveStartTime =  Time.time;
+                        _dissolveEndTime = Time.time + dissolveDuration;
+                        _collider.enabled = false;
+                    }
+                    else
+                    {
+                        _state = EnemyState.Hitstunned;
+                    }
+                    
                 }
                 break;
             }
             
+            // clean code is a myth anyway
+            case EnemyState.Dying:
             case EnemyState.Hitstunned:
             {
                 float progress = Mathf.InverseLerp(_hitstunStartTime,  _hitstunEndTime, Time.time);
-                float speedMultiplier = Mathf.Pow((1 - progress), recoilFalloffPower);
+                float speedMultiplier = Mathf.Pow(1f - progress, recoilFalloffPower);
 
                 _rb.linearVelocity = recoilSpeed * speedMultiplier * _recoilDirection;
                 if (progress >= 1f)
                 {
                     _rb.linearVelocity = Vector2.zero;
-                    _state = EnemyState.Alive;
-                    _spriteRenderer.color = _normalColor;
+                    
+                    if (_state == EnemyState.Hitstunned)
+                    {
+                        _state = EnemyState.Alive;
+                        _materialProperties.SetFloat(FlashAmountId, 0f);
+                        _spriteRenderer.SetPropertyBlock(_materialProperties);
+                    }
                 }
                 
                 break;
