a342adeb8f283e6b4d4f8b0970ff7dbe3642ffa0
ryan-tzw <102912851+ryan-tzw@users.noreply.github.com>
2026-09-26 20:33:04 +0800

    fix attack sprite not displaying in preview

--- Assets/_Game/Player/MeleeAttack.cs
+++ Assets/_Game/Player/MeleeAttack.cs
@@ -7,7 +7,11 @@ public class MeleeAttack : MonoBehaviour
 
     public void Spawn(Vector3 direction, int comboIndex)
     {
-        transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        // this is really dumb but i have to make the sprite stand upright (aligned on the XY plane) so that 
+        // it will actually display in Unity's Project preview because they just assume all sprites are aligned to XY
+        // which means i have to rotate it here to align it back to the XZ plane for our actual use case
+        // i could ignore the issue but it was bugging me that the preview was empty and its not that hard a fix so...
+        transform.rotation = Quaternion.LookRotation(direction, Vector3.up) * Quaternion.Euler(90f, 0f, 0f);
         Destroy(gameObject, comboIndex == 2 ? _finisherLifetime : _lifetime);
     }
 }
