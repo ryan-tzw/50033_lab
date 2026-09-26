61814ce5a581e98332f8331e5da28581c9ab4d7d
ryan-tzw <102912851+ryan-tzw@users.noreply.github.com>
2026-09-26 12:04:06 +0800

    add animations

--- Assets/_Game/Player/PlayerMovement.cs
+++ Assets/_Game/Player/PlayerMovement.cs
@@ -1,43 +1,44 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Experimental
+public class PlayerMovement : MonoBehaviour
 {
-    public class PlayerMovement : MonoBehaviour
-    {
-        public float moveSpeed = 5.0f;
+    public float moveSpeed = 5.0f;
 
-        private SpriteRenderer _spriteRenderer;
-        private PlayerInput _playerInput;
-        private InputActionMap _combatActions;
-        private InputAction _moveAction;
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+    private static readonly int IsMovingId = Animator.StringToHash("IsMoving");
+    
+    private PlayerInput _playerInput;
+    private InputActionMap _combatActions;
+    private InputAction _moveAction;
 
-        private Rigidbody _rb;
+    private Rigidbody _rb;
         
-        private void Awake()
-        {
-            _rb = GetComponent<Rigidbody>();
-            _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-            _playerInput = GetComponent<PlayerInput>();
-            _combatActions = _playerInput.actions.FindActionMap("Combat");
-            _moveAction = _combatActions.FindAction("Move");
-        }
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        _animator = GetComponentInChildren<Animator>();
+        _playerInput = GetComponent<PlayerInput>();
+        _combatActions = _playerInput.actions.FindActionMap("Combat");
+        _moveAction = _combatActions.FindAction("Move");
+    }
 
-        private void Update()
+    private void Update()
+    {
+        var move = _moveAction.ReadValue<Vector2>();
+        _animator.SetBool(IsMovingId, move.sqrMagnitude > 0.01f);
+        if (move.x != 0)
         {
-            var move = _moveAction.ReadValue<Vector2>();
-            if (move.x != 0)
-            {
-                _spriteRenderer.flipX = move.x < 0;
-            }
+            _spriteRenderer.flipX = move.x < 0;
         }
+    }
 
-        private void FixedUpdate()
-        {
-            var moveDir = _moveAction.ReadValue<Vector2>();
-            var hVelocity = Vector3.ClampMagnitude(new Vector3(moveDir.x, 0, moveDir.y), 1f) * moveSpeed;
-            _rb.linearVelocity = new Vector3(hVelocity.x, _rb.linearVelocity.y, hVelocity.z);
-        }
+    private void FixedUpdate()
+    {
+        var moveDir = _moveAction.ReadValue<Vector2>();
+        var hVelocity = Vector3.ClampMagnitude(new Vector3(moveDir.x, 0, moveDir.y), 1f) * moveSpeed;
+        _rb.linearVelocity = new Vector3(hVelocity.x, _rb.linearVelocity.y, hVelocity.z);
     }
 }
